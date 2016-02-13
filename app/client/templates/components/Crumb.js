17917d3a04709b2b13002574fe4a686e2d1cdd52
d-tsui <dtsui918@yahoo.com>
2016-02-13 04:59:40 -0800

    Crumb replies

--- app/client/templates/components/Crumb.js
+++ app/client/templates/components/Crumb.js
@@ -1,8 +1,30 @@
 Template.Crumb.helpers({
   crumb: function(){
-    return Crumbs.findOne({_id: Router.current().params._id});
+    var crumb = Crumbs.findOne({_id: Router.current().params._id});
+    crumb.timestamp = moment(crumb.time).fromNow(true);
+    return crumb;
   },
   comments: function(){
-    return Crumbs.find({crumbId})
+    var comments = Comments.find({crumbId: Router.current().params._id}).fetch();
+    _.map(comments, function(comment){
+      comment.timestamp = moment(comment.time).fromNow(true);
+    });
+    return comments;
+  },
+  noComments: function(){
+    return Comments.find({crumbId: Router.current().params._id}).count() == 0;
+  }
+});
+
+Template.Crumb.events({
+  "click #replySubmit": function(){
+    var comment = {};
+    comment.type = "text";
+    comment.content = document.getElementById("replyText").value;
+    comment.crumbId = Router.current().params._id;
+    if (comment.content){
+      Meteor.call("postComment", comment);
+      document.getElementById("replyText").value = "";
+    }
   }
 });
--- app/server/commentsMethods.js
+++ app/server/commentsMethods.js
@@ -1,17 +1,17 @@
 Meteor.methods({
-  postComments : function(comment){
+  postComment : function(comment){
     comment.time = new Date();
     comment.userId = Meteor.userId();
     comment.flag = [];
     Comments.insert(comment);
   },
-  flagComments : function(commentId){
+  flagComment : function(commentId){
     var userId = Meteor.userId();
     if (Comments.find({'_id':commentId, 'flag':{'$in':[userId]}}).count() == 0){
       Comments.update({'_id':commentId}, {'$push':{'flag':userId}});
     }
   },
-  deleteComments : function(commentId){
+  deleteComment : function(commentId){
     var userId = Meteor.userId();
     if (Comments.findOne({'_id':commentId})["userId"] == userId){
       Comments.update({'_id':commentId}, {'$set':{'hidden':1}});
